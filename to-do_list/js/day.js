6ed7d8787dc677900f6d6209e9aefb53870a5749
test <test@example.com>
2022-11-27 00:17:55 +0300

    Finished From User InterFace

---
+++ to-do_list/js/day.js
@@ -0,0 +1,23 @@
+const tasks = [
+    {
+        "title": "read a book",
+        "date": "12/11/2022",
+        "from": "8:00",
+        "to": "10:00",
+        "isDone": false
+    },
+    {
+        "title": "read a book",
+        "date": "12/11/2022",
+        "from": "8:00",
+        "to": "10:00",
+        "isDone": true
+    },
+    {
+        "title": "read a book",
+        "date": "12/11/2022",
+        "from": "8:00",
+        "to": "10:00",
+        "isDone": false
+    }
+];
