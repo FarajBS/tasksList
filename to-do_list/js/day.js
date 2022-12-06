c1c29f2f67c63beb3da531fac3a5e972bda6a80f
test <test@example.com>
2022-12-06 08:12:52 +0300

    Finish The Days Tasks

--- to-do_list/js/day.js
+++ to-do_list/js/day.js
@@ -1,27 +1,5 @@
 // BEGIN LIST OF TASKS // 
-let tasks = [
-    {
-        "title": "read a book",
-        "date": "12/11/2022",
-        "from": "8:00",
-        "to": "10:00",
-        "isDone": false
-    },
-    {
-        "title": "read a book",
-        "date": "12/11/2022",
-        "from": "8:00",
-        "to": "10:00",
-        "isDone": true
-    },
-    {
-        "title": "read a book",
-        "date": "12/11/2022",
-        "from": "8:00",
-        "to": "10:00",
-        "isDone": false
-    }
-];
+let tasks = [];
 //== BEGIN LIST OF TASKS ==// 
 // --------------------------------------------------------------------------------------------------------------------- //
 // --------------------------------------------------------------------------------------------------------------------- //
@@ -58,50 +36,54 @@ function fillTasksOnThePage()
         let content =
         `
             <!-- BEGIN: LIST -->
-            <div id="list" class="row mt-3 p-3 rounded-pill">
-                <div class="col-sm-4" style="margin: auto;">
-                    <b>
-                        ${task.title} 
-                    </b>
-                    <div class="mt-1" style="font-size: 1vw;">
-                        <span class="material-symbols-outlined" style="padding-left: 0.3vw;">
-                            calendar_month
-                        </span>
-                        <span>${task.date}</span>
-                    </div>
-                </div>
-                <div class="col-sm-2" style="margin: auto;">${task.from ?? ""}</div>
-                <div class="col-sm-2" style="margin: auto;">${task.to ?? ""}</div>
-                <div class="col-sm-4" style="margin: auto;">
-                    <div class="row">
-                        ${task.isDone ?
-                        ` 
-                            <div class="col-sm-4" style="padding: 0;">
-                                <div onclick="toggleTaskcompletion(${index})" class="center-list d-flex justify-content-center align-items-center" style="background-color: green;">
-                                    <span class="material-symbols-outlined">
-                                        cancel
-                                    </span>
-                                </div>
-                        `:`
-                                <div onclick="toggleTaskcompletion(${index})" class="center-list d-flex justify-content-center align-items-center" style="background-color: green;">
-                                    <span class="material-symbols-outlined">
-                                        done
-                                    </span>
-                                </div>
-                            </div>
-                        `}
-                        <div class="col-sm-4" style="padding: 0;">
-                            <div onclick="editTask(${index})" class="center-list d-flex justify-content-center align-items-center" style="background-color: blue;">
-                                <span class="material-symbols-outlined">
-                                    edit
-                                </span>
-                            </div>
+            <div id="list">
+                <div id="list-form" class="row mt-3 p-5 rounded-pill">
+                    <div class="col-sm-4" style="margin: auto; overflow: auto">
+                        <b>
+                            ${task.title} 
+                        </b>
+                        <div class="mt-1" style="font-size: 1vw;">
+                            <span class="material-symbols-outlined" style="padding-left: 0.3vw;">
+                                calendar_month
+                            </span>
+                            <span>${task.date}</span>
                         </div>
-                        <div class="col-sm-4" style="padding: 0;">
-                            <div onclick="deleteTask(${index})" class="center-list d-flex justify-content-center align-items-center" style="background-color: red;">
-                                <span class="material-symbols-outlined">
-                                    delete
-                                </span>
+                    </div>
+                    <div class="col-sm-2" style="margin: auto;">${task.from ?? ""}</div>
+                    <div class="col-sm-2" style="margin: auto;">${task.to ?? ""}</div>
+                    <div class="col-sm-4 style="margin: auto;">
+                        <div class="row d-flex justify-content-center align-items-center" style="width: 100%; height: 100%; margin-right: 2vw;">
+                            ${task.isDone ?
+                            ` 
+                                <div class="col-sm-4">
+                                    <div onclick="toggleTaskcompletion(${index})" class="center-list" style="background-color: green;">
+                                        <span class="material-symbols-outlined">
+                                            done
+                                        </span>
+                                    </div>
+                                </div>
+                            `:`
+                                <div class="col-sm-4">
+                                    <div onclick="toggleTaskcompletion(${index})" class="center-list" style="background-color: red">
+                                        <span class="material-symbols-outlined">
+                                            cancel
+                                        </span>
+                                    </div>
+                                </div>
+                            `}
+                            <div class="col-sm-4">
+                                <div onclick="editTask(${index})" class="center-list" style="background-color: blue;">
+                                    <span class="material-symbols-outlined">
+                                        edit
+                                    </span>
+                                </div>
+                            </div>
+                            <div class="col-sm-4"">
+                                <div onclick="deleteTask(${index})" class="center-list" style="background-color: rgb(110, 0, 114);">
+                                    <span class="material-symbols-outlined">
+                                        delete
+                                    </span>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -187,14 +169,57 @@ function toggleTaskcompletion(index)
 // ===================================================================================================================== //
 // ===================================================================================================================== //
 // UPDATE TASK FUNCTION //
+function editTask(index) 
+{
+    let task = tasks[index];
 
+    document.getElementById('task-form').style.visibility = "visible";
+
+    // SUBMIT BUTTON //
+    document.getElementById('submit-btn').addEventListener("click", function()
+    {
+        let newTaskTitle = document.getElementById('taskTitle').value;
+        let newTimeFrom =  document.getElementById('time-from').value;
+        let newTimeTo =  document.getElementById('time-to').value;
+        if(newTaskTitle != "" && newTimeFrom != "" && newTimeTo != "")
+        {
+            task.title = newTaskTitle
+            task.from = newTimeFrom 
+            task.to = newTimeTo 
+
+            setItemTasksToStorage();
+            fillTasksOnThePage();
+            
+            document.getElementById('task-form').style.visibility = "hidden";
+        } else{
+            alert('لم تقوم بكامل التعديلات')
+        }
+    });
+    //== SUBMIT BUTTON ==//
+    // ================================================================================================================= //
+    // CANCEL BUTTON //
+    document.getElementById('cancel-btn').addEventListener("click", function()
+    {
+        document.getElementById('task-form').style.visibility = "hidden";
+
+    }) 
+}
+    //== CANCEL BUTTON ==//
 //== UPDATE TASK FUNCTION ==//
 // ===================================================================================================================== //
 // ===================================================================================================================== //
 // DELETE TASK FUNCTION //
-
+function deleteTask(index) {
+    let task = tasks[index];
+    let isConfirmed = confirm(`هل أنت متأكد من حذف هذه المهمة [${task.title}]`);
+    if(isConfirmed) {
+        tasks.splice(index, 1);
+        
+        setItemTasksToStorage();
+        fillTasksOnThePage();
+    }
+};
 //== DELETE TASK FUNCTION ==//
 // ===================================================================================================================== //
 // ===================================================================================================================== //
 //== CRUD ==//
-
